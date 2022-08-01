78c9b512fa6b9b60a9a5a16c6292035abb8822b6
Ahmed Youssef <ahmed.youssef@insight.com>
2022-08-01 13:36:56 +1000

    ADO/Github self-hosted agent/runner on ACI

---
+++ ado-agent-linux/start.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+
+# ref: https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/docker?view=azure-devops#create-and-build-the-dockerfile-1
+
+if [ -z "$AZP_URL" ]; then
+  echo 1>&2 "error: missing AZP_URL environment variable"
+  exit 1
+fi
+
+if [ -z "$AZP_TOKEN_FILE" ]; then
+  if [ -z "$AZP_TOKEN" ]; then
+    echo 1>&2 "error: missing AZP_TOKEN environment variable"
+    exit 1
+  fi
+
+  AZP_TOKEN_FILE=.token
+  echo -n $AZP_TOKEN > "$AZP_TOKEN_FILE"
+fi
+
+unset AZP_TOKEN
+
+if [ -n "$AZP_WORK" ]; then
+  mkdir -p "$AZP_WORK"
+fi
+
+export AGENT_ALLOW_RUNASROOT="1"
+
+cleanup() {
+  if [ -e config.sh ]; then
+    print_header "Cleanup. Removing Azure Pipelines agent..."
+
+    # If the agent has some running jobs, the configuration removal process will fail.
+    # So, give it some time to finish the job.
+    while true; do
+      ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+
+      echo "Retrying in 30 seconds..."
+      sleep 30
+    done
+  fi
+}
+
+print_header() {
+  lightcyan='\033[1;36m'
+  nocolor='\033[0m'
+  echo -e "${lightcyan}$1${nocolor}"
+}
+
+# Let the agent ignore the token env variables
+export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
+export AZP_AGENT_USE_LEGACY_HTTP=true
+
+source ./env.sh
+
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+print_header "1. Configuring Azure Pipelines agent..."
+
+./config.sh --unattended \
+  --agent "${NAME:-$(hostname)}" \
+  --url "$AZP_URL" \
+  --auth PAT \
+  --token $(cat "$AZP_TOKEN_FILE") \
+  --pool "${AZP_POOL:-Default}" \
+  --work "${AZP_WORK:-_work}" \
+  --replace \
+  --acceptTeeEula & wait $!
+
+print_header "2. Running Azure Pipelines agent..."
+
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+chmod +x ./run.sh
+
+# To be aware of TERM and INT signals call run.sh
+# Running it with the --once flag at the end will shut down the agent after the build is executed
+./run.sh "$@" & wait $!
---
+++ github-runner-linux/start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+  echo 1>&2 "GITHUB_REPOSITORY is not set"
+  exit 1
+fi
+
+if [ -z "$GITHUB_TOKEN_FILE" ]; then
+  if [ -z "$GITHUB_TOKEN" ]; then
+    echo 1>&2 "error: missing GITHUB_TOKEN environment variable"
+    exit 1
+  fi
+
+  GITHUB_TOKEN_FILE=.token
+  echo -n $GITHUB_TOKEN > "$GITHUB_TOKEN_FILE"
+fi
+
+unset GITHUB_TOKEN
+
+GITHUB_REPOSITORY=$GITHUB_REPOSITORY
+GITHUB_TOKEN=$(cat "$GITHUB_TOKEN_FILE")
+NAME=${NAME:-"hosted-runner-01"}
+
+print_header() {
+  lightcyan='\033[1;36m'
+  nocolor='\033[0m'
+  echo -e "${lightcyan}$1${nocolor}"
+}
+
+print_header "1. Getting github runner registration token..."
+
+TOKEN=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runners/registration-token | jq -r .token)
+
+print_header "2. Configuring github actions runner..."
+
+./config.sh \
+  --unattended \
+  --name $NAME \
+  --url https://github.com/$GITHUB_REPOSITORY \
+  --token $TOKEN
+
+cleanup() {
+    print_header "Removing github actions runner..."
+    ./config.sh remove \
+      --unattended \
+      --token $TOKEN
+}
+
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+print_header "3. Running github actions runner..."
+
+./run.sh & wait $!
